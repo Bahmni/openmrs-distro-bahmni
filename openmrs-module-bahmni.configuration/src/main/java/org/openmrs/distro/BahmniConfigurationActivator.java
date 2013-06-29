70ae0c87aff930b9447b5ae729cd3b16a1daa8ce
Pulkit <pulkit.bosco05@gmail.com>
2013-06-29 22:15:52 +0530

    Putting in some OpenMRS specific required module files.

---
+++ openmrs-module-bahmni.configuration/src/main/java/org/openmrs/distro/BahmniConfigurationActivator.java
@@ -0,0 +1,53 @@
+package org.openmrs.distro;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.ModuleActivator;
+
+public class BahmniConfigurationActivator implements ModuleActivator {
+
+    protected Log log = LogFactory.getLog(getClass());
+
+    /**
+     * @see ModuleActivator#willRefreshContext()
+     */
+    public void willRefreshContext() {
+        log.info("Refreshing Registration App for Bhamni Module");
+    }
+
+    /**
+     * @see ModuleActivator#contextRefreshed()
+     */
+    public void contextRefreshed() {
+        log.info("Registration App for Bhamni Module refreshed");
+    }
+
+    /**
+     * @see ModuleActivator#willStart()
+     */
+    public void willStart() {
+        log.info("Starting Registration App for Bhamni Module");
+    }
+
+    /**
+     * @see ModuleActivator#started()
+     */
+    public void started() {
+        log.info("Registration App for Bhamni Module started");
+    }
+
+    /**
+     * @see ModuleActivator#willStop()
+     */
+    public void willStop() {
+        log.info("Stopping Registration App for Bhamni Module");
+    }
+
+    /**
+     * @see ModuleActivator#stopped()
+     */
+    public void stopped() {
+        log.info("Registration App for Bhamni Module stopped");
+    }
+
+}
