298aea7226676aa269e10502d100235ead5dfbf4
MOHANKUMAR T <mohan13081999@gmail.com>
2022-02-24 16:01:03 +0530

    BAH-1412 | added. separate workflow to build openmrs demoDB image
    Co-authored-by: Abinaya U <abinaya.u@thoughtworks.com>
    Co-authored-by: Arjun G <arjun.g@thoughtworks.com>
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>
    Co-authored-by: Soorya Kumaran C <sooryakumaran.c@thoughtworks.com>
    Co-authored-by: Umair Fayaz <omayrfayaz@gmail.com>

---
+++ package/docker/scripts/docker_build_openmrs_demoDB.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+# Using Database Backup data from bahmni-scripts repo
+gunzip -f -k bahmni-scripts/demo/db-backups/v0.92/openmrs_backup.sql.gz
+cp bahmni-scripts/demo/db-backups/v0.92/openmrs_backup.sql package/resources/openmrs_demo_dump.sql
+
+# Building Docker images
+OPENMRS_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
+docker build -t bahmni/openmrs-db:demo-${OPENMRS_IMAGE_TAG} -f package/docker/demodb.Dockerfile  . --no-cache
