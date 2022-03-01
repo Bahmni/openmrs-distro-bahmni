9c6a0e1363256b393cce93883119d8d1090bdba9
Soorya Kumaran C <90232857+SooryaKumaranC-tw@users.noreply.github.com>
2022-03-01 18:23:41 +0530

    BAH-1412 | Refactor docker build and publish using github actions
    Co-authored-by: Arjun G <arjun.g@thoughtworks.com>

--- package/docker/freshDB/docker_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xe
-
-# Building Docker images
-OPENMRS_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
-docker build -t bahmni/openmrs-db:fresh-${OPENMRS_IMAGE_TAG} -t bahmni/openmrs-db:fresh-latest -f package/docker/freshDB/Dockerfile  . --no-cache
