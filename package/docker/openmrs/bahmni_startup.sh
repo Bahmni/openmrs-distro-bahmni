695c7f891aa0c2b60c50f4fb05e87d5666c44ccf
MOHANKUMAR T <mohan13081999@gmail.com>
2022-05-19 19:46:51 +0530

    Fix. Remove database name hardcoding in start script

--- package/docker/openmrs/bahmni_startup.sh
+++ package/docker/openmrs/bahmni_startup.sh
@@ -4,10 +4,10 @@ set -e
 # This script is used to run startup commands before starting OpenMRS distro startup script that comes with docker image. The last line starts OpenMRS script.
 echo "Running Bahmnni EMR Startup Script..."
 
-CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock -DschemaName=openmrs"
+CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock -DschemaName=${DB_DATABASE}"
 LIQUIBASE_JAR="/etc/bahmni-lab-connect/liquibase-core.jar"
 DRIVER="com.mysql.jdbc.Driver"
-URL="jdbc:mysql://$DB_HOST:3306/openmrs --username=$DB_USERNAME --password=$DB_PASSWORD"
+URL="jdbc:mysql://$DB_HOST:3306/${DB_DATABASE} --username=$DB_USERNAME --password=$DB_PASSWORD"
 
 echo "Substituting Environment Variables..."
 envsubst < /etc/bahmni-emr/templates/bahmnicore.properties.template > /usr/local/tomcat/.OpenMRS/bahmnicore.properties
