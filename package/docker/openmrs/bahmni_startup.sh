c9747d759c147762e0bddce7594eb8f0382f1e51
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2022-05-19 09:50:08 +0530

    BAH-1303 | fix. move soft link creation to Dockerfile (#37)

--- package/docker/openmrs/bahmni_startup.sh
+++ package/docker/openmrs/bahmni_startup.sh
@@ -16,14 +16,6 @@ envsubst < /etc/bahmni-emr/templates/bahmnicore.properties.template > /usr/local
 # Running Atomfeed Migrations
 java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --url=$URL --username=$DB_USERNAME --password=$DB_PASSWORD --classpath=/etc/bahmni-lab-connect/atomfeed-client.jar:/usr/local/tomcat/webapps/openmrs.war --changeLogFile=sql/db_migrations.xml update
 
-# Setting Soft Links from bahmni_config (Moved from bahmni-web postinstall)
-ln -s /etc/bahmni_config/openmrs/obscalculator /usr/local/tomcat/.OpenMRS/obscalculator
-ln -s /etc/bahmni_config/openmrs/ordertemplates /usr/local/tomcat/.OpenMRS/ordertemplates
-ln -s /etc/bahmni_config/openmrs/encounterModifier /usr/local/tomcat/.OpenMRS/encounterModifier
-ln -s /etc/bahmni_config/openmrs/patientMatchingAlgorithm /usr/local/tomcat/.OpenMRS/patientMatchingAlgorithm
-ln -s /etc/bahmni_config/openmrs/elisFeedInterceptor /usr/local/tomcat/.OpenMRS/elisFeedInterceptor
-ln -s /etc/bahmni_config /usr/local/tomcat/.OpenMRS/bahmni_config
-
 # Running Migrations from bahmni_config (Moved from bahmni-web postinstall)
 cd /etc/bahmni_config/openmrs/migrations/
 java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --url=$URL --username=$DB_USERNAME --password=$DB_PASSWORD --classpath=/usr/local/tomcat/webapps/openmrs.war --changeLogFile=liquibase.xml update
