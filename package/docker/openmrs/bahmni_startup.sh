f3d7bad65b8a1e458989c1de86bd6b5ed1acfc1c
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2022-06-13 16:03:42 +0530

    BAH-1809 | refactor. remove db setup script from docker configuration (#41)
    * BAH-1809 | refactor. remove db setup script from docker config
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    * BAH-1809 | fix. remove unused variables from helm definition
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>
    
    Co-authored-by: stavtnameh <hemanth.v@beehyv.com>

--- package/docker/openmrs/bahmni_startup.sh
+++ package/docker/openmrs/bahmni_startup.sh
@@ -13,16 +13,8 @@ echo "Substituting Environment Variables..."
 envsubst < /etc/bahmni-emr/templates/bahmnicore.properties.template > /usr/local/tomcat/.OpenMRS/bahmnicore.properties
 /usr/local/tomcat/wait-for-it.sh --timeout=3600 ${DB_HOST}:3306
 
-#DB Setup
-./dbSetup.sh
-
 # Running Atomfeed Migrations
 java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --url=$URL --username=$DB_USERNAME --password=$DB_PASSWORD --classpath=/etc/bahmni-lab-connect/atomfeed-client.jar:/usr/local/tomcat/webapps/openmrs.war --changeLogFile=sql/db_migrations.xml update
-
-# Running Migrations from bahmni_config (Moved from bahmni-web postinstall)
-cd /etc/bahmni_config/openmrs/migrations/
-java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --url=$URL --username=$DB_USERNAME --password=$DB_PASSWORD --classpath=/usr/local/tomcat/webapps/openmrs.war --changeLogFile=liquibase.xml update
-cd -
 
 echo "Running OpenMRS Startup Script..."
 ./startup.sh
