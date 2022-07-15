ead30aba1d93c5212b297ee6e531007f6a923705
Himabindu T <tbindu@thoughtworks.com>
2022-07-15 17:03:03 +0530

    Bindu, Rohit | BAH-1975 | Upgrade OpenMRS version from 2.1.7 to 2.4.2 (#50)
    * Bah 1649 - Upgrades openmrs version to 2.4.2 and upgrades module versions compatible to openmrs:2.4.2 (#44)
    
    * OMRS-master-2.4 - | Rohit | - Upgrades openmrs version to 2.4.2
    
    * OMRS-master-2.4 - | Rohit | - Upgdates branch name as OMRS-master-2.4 in github workflow and upgrades modules version compatible with respect to openmrs:2.4.2
    
    * BAH-1649 - | Rohit | - WIP: Comments helm-package-publish code
    
    * BAH-1649 - | Rohit | - Upgrades operation-theatre version to 1.5.0 after relaease with OMRS:2.4.2 upgrade
    
    * BAH-1649 - | Rohit | - Upgrades pacsquery version to 1.3.1-SNAPSHOT
    
    * BAH-1649 - | Rohit | - Upgrades versions for bahmni-ie-apps to 1.3.0.1-SNAPSHOT, bacteriology to 1.3.0.1-SNAPSHOT and pacsquery to 1.3.0.1-SNAPSHOT
    
    * BAH-1649 - | Rohit | - Upgrades auditlog version to 1.3.0.1-SNAPSHOT and operation-theatre to 1.6.0
    
    * BAH-1649 - | Bindu, Rohit | - Updates bahmni-core version to 0.94.1-SNAPSHOT
    
    * BAH-1649 - | Bindu, Rohit | - Updates openmrs-atomfeed version to 2.6.2-SNAPSHOT
    
    * BAH-1649 - | Bindu, Rohit | - Updates mail-appender version to 0.94.3.1-SNAPSHOT
    
    * Bindu | BAH-1649 | change bacteriology version back 1.2.0 as latest snapshot version is not available
    
    * BAH-1649 - | Rohit | - Upgrades bacteriology version to 1.3.0 (#46)
    
    * OMRS-master-2.4 - | Bindu, Rohit | - Upgrades openmrs-distro-platform to 2.4.2
    
    * OMRS-master-2.4 - | Bindu, Rohit | - Changes reference of fhir modules from fhir-*.omod to fhir*.omod
    
    * OMRS-master-2.4 - | Bindu, Rohit | - Upgrades liquibase version to 3.10.2 for Openmrs:2.4.2 openmrs upgrade in Dockerfile
    
    * OMRS-master-2.4 - | Bindu, Rohit | - Comments running atomfeed-migrations due to excluded dependency logback-classic from liquibase-core in openmrs core
    
    * BAH-1992 - | Rohit | - Adds liquibase-runtime.properties file with adding zeroDateTimeBehavior attrib to resolve zero date value exception (#49)
    
    * Bindu, Rohit | BAH-1975 | update artefactId in the distro pom as per master branch
    
    * BAH-1975 - | Rohit | - Adds ot, webservices and fhir2extension modules with OMRS:2.4.2 upgrade
    
    * Bindu | BAH-1975 | comment liquibase jar command as its failing NoClassDef error
    
    * BAH-1975 - | Bindu, Rohit | - Updates bahmni.ie.apps version to 1.3.0-SNAPSHOT
    
    * BAH-1975 - | Bindu, Rohit | - Updates fhir2 version to 1.4.0
    
    * BAH-1975 - | Rohit | - Upgrades fhir2Extension module with fhir2 1.4.0 version
    
    * BAH-1975 - | Bindu, Rohit | - Uses env vars in openmrs-runtime.properties file
    
    * Bindu | BAH-1975 | upgrade owa version to 1.13.0
    
    * BAH-1975 - | Rohit | - Uses template for openmrs-runtime.properties file
    
    Co-authored-by: rohit-yawalkar <82825674+rohit-yawalkar@users.noreply.github.com>

--- package/docker/openmrs/bahmni_startup.sh
+++ package/docker/openmrs/bahmni_startup.sh
@@ -11,10 +11,11 @@ URL="jdbc:mysql://$DB_HOST:3306/${DB_DATABASE} --username=$DB_USERNAME --passwor
 
 echo "Substituting Environment Variables..."
 envsubst < /etc/bahmni-emr/templates/bahmnicore.properties.template > /usr/local/tomcat/.OpenMRS/bahmnicore.properties
+envsubst < /etc/bahmni-emr/templates/openmrs-runtime.properties.template > /usr/local/tomcat/.OpenMRS/openmrs-runtime.properties
 /usr/local/tomcat/wait-for-it.sh --timeout=3600 ${DB_HOST}:3306
 
 # Running Atomfeed Migrations
-java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --url=$URL --username=$DB_USERNAME --password=$DB_PASSWORD --classpath=/etc/bahmni-lab-connect/atomfeed-client.jar:/usr/local/tomcat/webapps/openmrs.war --changeLogFile=sql/db_migrations.xml update
+# java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --url=$URL --username=$DB_USERNAME --password=$DB_PASSWORD --classpath=/etc/bahmni-lab-connect/atomfeed-client.jar:/usr/local/tomcat/webapps/openmrs.war --changeLogFile=sql/db_migrations.xml update
 
 echo "Running OpenMRS Startup Script..."
 ./startup.sh
