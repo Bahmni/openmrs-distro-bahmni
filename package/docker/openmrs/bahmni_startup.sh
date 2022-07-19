14ba2a6d2fe22e8e0c3d040307243d278263d405
ayush0260 <ayush@beehyv.com>
2022-07-19 19:06:13 +0530

    added slf4j and logback core to classpath

--- package/docker/openmrs/bahmni_startup.sh
+++ package/docker/openmrs/bahmni_startup.sh
@@ -4,10 +4,16 @@ set -e
 # This script is used to run startup commands before starting OpenMRS distro startup script that comes with docker image. The last line starts OpenMRS script.
 echo "Running Bahmnni EMR Startup Script..."
 
+JAR_DIR="/etc/bahmni-lab-connect"
+LIQUIBASE_JAR="$JAR_DIR/liquibase-core.jar"
+ATOMFEED_CLIENT_JAR="$JAR_DIR/atomfeed-client.jar"
+LOGBACK_CORE_JAR="$JAR_DIR/logback-core.jar"
+SLF4J_API_JAR="$JAR_DIR/slf4j-api.jar"
+
 CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock -DschemaName=${DB_DATABASE}"
-LIQUIBASE_JAR="/etc/bahmni-lab-connect/liquibase-core.jar"
-DRIVER="com.mysql.jdbc.Driver"
+DRIVER="com.mysql.cj.jdbc.Driver"
 URL="jdbc:mysql://$DB_HOST:3306/${DB_DATABASE} --username=$DB_USERNAME --password=$DB_PASSWORD"
+CLASS_PATH="$LIQUIBASE_JAR:$ATOMFEED_CLIENT_JAR:/usr/local/tomcat/webapps/openmrs.war:$LOGBACK_CORE_JAR:$SLF4J_API_JAR"
 
 echo "Substituting Environment Variables..."
 envsubst < /etc/bahmni-emr/templates/bahmnicore.properties.template > /usr/local/tomcat/.OpenMRS/bahmnicore.properties
@@ -15,7 +21,7 @@ envsubst < /etc/bahmni-emr/templates/openmrs-runtime.properties.template > /usr/
 /usr/local/tomcat/wait-for-it.sh --timeout=3600 ${DB_HOST}:3306
 
 # Running Atomfeed Migrations
-# java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --url=$URL --username=$DB_USERNAME --password=$DB_PASSWORD --classpath=/etc/bahmni-lab-connect/atomfeed-client.jar:/usr/local/tomcat/webapps/openmrs.war --changeLogFile=sql/db_migrations.xml update
+java $CHANGE_LOG_TABLE -cp $CLASS_PATH liquibase.integration.commandline.Main --driver=$DRIVER --url=$URL --username=$DB_USERNAME --password=$DB_PASSWORD --classpath=$ATOMFEED_CLIENT_JAR:/usr/local/tomcat/webapps/openmrs.war --changeLogFile=sql/db_migrations.xml update
 
 echo "Running OpenMRS Startup Script..."
 ./startup.sh
