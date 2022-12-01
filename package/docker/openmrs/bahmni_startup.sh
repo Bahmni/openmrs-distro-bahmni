3cf0489c6d16aaf0664bba9e0561fd422c529cb3
Umair Fayaz <omayrfayaz@gmail.com>
2022-12-01 16:49:57 +0530

    Fix | Remove duplicate configuration folder

--- package/docker/openmrs/bahmni_startup.sh
+++ package/docker/openmrs/bahmni_startup.sh
@@ -13,7 +13,7 @@ envsubst < /etc/bahmni-emr/templates/mail-config.properties.template > ${OPENMRS
 echo "Copy Configuration Folder from bahmni_config"
 if [ -d /etc/bahmni_config/masterdata/configuration ]
 then
-  cp -r /etc/bahmni_config/masterdata/configuration/ ${OPENMRS_APPLICATION_DATA_DIRECTORY}/configuration/
+  cp -r /etc/bahmni_config/masterdata/configuration/ ${OPENMRS_APPLICATION_DATA_DIRECTORY}/
 fi
 mysql --host="${OMRS_DB_HOSTNAME}" --user="${OMRS_DB_USERNAME}" --password="${OMRS_DB_PASSWORD}" "${OMRS_DB_NAME}" -e "UPDATE global_property SET global_property.property_value = '' WHERE  global_property.property = 'search.indexVersion';" || true
 
