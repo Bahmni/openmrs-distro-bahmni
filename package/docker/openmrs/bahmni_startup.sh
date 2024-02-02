c0005741b0dfed191222a66a2feed70811addbd2
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-02-02 19:34:08 +0530

    [Rahul] | BAH-3528 | Add. uploaded-files Sub Directory And Set Permissions

--- package/docker/openmrs/bahmni_startup.sh
+++ package/docker/openmrs/bahmni_startup.sh
@@ -25,6 +25,7 @@ then
 echo "setting the folder permissions"
 setfacl -R -d -m o::rx -m g::rx /home/bahmni/document_images
 setfacl -R -d -m o::rx -m g::rx /home/bahmni/uploaded_results
+setfacl -R -d -m o::rx -m g::rx /home/bahmni/uploaded-files
 fi
 
 echo "Running OpenMRS Startup Script..."
